6500feee2e485e001f8bcfff85238f2c3eeedbfa
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-10-29 19:49:10 +0330

    CustomDS worked, convergence problem is likely due to weight decay

--- runs.sh
+++ runs.sh
@@ -4,3 +4,4 @@
 # python scripts/train_coco.py --lr 0.005 --epochs 2 --lr_decay_steps '(1, )' --model_c 50 --log_path '../exps/test/Madry adversarial R50 w Sigma=3 lr 0.05' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50.pt'
 
 python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/UsingMyCustomDataset_AndOtherSetting_Madry_adversarial_eps8_R50_Sigma2' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50.pt'
+# python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/UsingMyCustomDataset_AndOtherSetting_ST_R50_Sigma2' --model_name 'poseresnet' --pretrained_weight_path './downloads/standard_resnet50.pth'
