7bbcd02d05f156995d5c2b39d71c833d1bf416b3
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-11-04 01:42:43 +0330

    +

--- runs.sh
+++ runs.sh
@@ -1,6 +1,6 @@
 # python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/UsingMyCustomDataset_AndOtherSetting_Madry_adversarial_eps8_R50_Sigma2' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50.pt'
 # python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/UsingMyCustomDataset_AndOtherSetting_ST_R50_Sigma2' --model_name 'poseresnet' --pretrained_weight_path './downloads/standard_resnet50.pth'
-python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path ../exps/UsingMyCustomDataset_AndOtherSetting_Madry_adversarial_eps4 --model_name poseresnet --pretrained_weight_path ''./downloads/madry_adversarial_resnet50_linf4.pt'
+python scripts/train_coco.py --batch_size 16 --weight_decay 0.0 --num_workers 4 --model_c 50 --image_resolution '(256, 192)' --log_path ../exps/UsingMyCustomDataset_AndOtherSetting_Madry_adversarial_eps4 --model_name poseresnet --pretrained_weight_path './downloads/madry_adversarial_resnet50_linf4.pt'
 
 # Benchmarking PGD acc
 # python scripts/test_coco_robustness.py --batch_size 16 --model_c 50 --model_name poseresnet --pretrained_weight_path '/mnt/data/hossein/Hossein_workspace/nips_cetra/hamed/exps/UsingMyCustomDataset_AndOtherSetting_Madry_adversarial_eps8_R50_Sigma2/20251029_1721/checkpoint_best_mAP.pth'
