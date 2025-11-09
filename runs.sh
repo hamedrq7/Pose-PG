580ada942fb40d5c1a969339ff9d83b2b2b97a94
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-11-09 20:32:59 +0330

    +

--- runs.sh
+++ runs.sh
@@ -15,5 +15,6 @@
 
 # Testing Adversarial Training 
 # Testing params
-python scripts/train_coco_adv.py --disable_rand_init --mix_st --epsilon 0.01568 --step_size 0.01568 --weight_decay 0.0 --num_steps 1 --batch_size 16 --num_workers 8 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/AdvTraining_FGSM_MIX_eps4' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50_linf4.pt'
+# python scripts/train_coco_adv.py --disable_rand_init --mix_st --epsilon 0.01568 --step_size 0.01568 --weight_decay 0.0 --num_steps 1 --batch_size 16 --num_workers 8 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/AdvTraining_FGSM_MIX_eps4' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50_linf4.pt'
+python scripts/train_coco_adv.py --mix_st --epsilon 0.01568 --step_size 0.00784 --weight_decay 0.0 --num_steps 1 --batch_size 16 --num_workers 8 --model_c 50 --image_resolution '(256, 192)' --log_path '../exps/AdvTraining_FAST_FGSM_RS_MIX_eps4_stepsize_2' --model_name 'poseresnet' --pretrained_weight_path './downloads/madry_adversarial_resnet50_linf4.pt'
 
